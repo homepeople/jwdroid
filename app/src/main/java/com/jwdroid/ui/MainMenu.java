c8ca720e08b50329408bb4374b72516f35b1f665
Artem Chivchalov <chivchalov@gmail.com>
2016-01-19 11:18:50 +0300

    Add "About" menu item

--- app/src/main/java/com/jwdroid/ui/MainMenu.java
+++ app/src/main/java/com/jwdroid/ui/MainMenu.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -128,6 +129,11 @@ public class MainMenu extends AppCompatActivity {
                 startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://plus.google.com/communities/112371364498094492171")));
                 break;
 
+            case R.id.menu_about:
+                DialogFragment dialog = new AboutDialog();
+                dialog.show(getSupportFragmentManager(), null);
+                break;
+
         }
         return super.onOptionsItemSelected(item);
     }
@@ -167,6 +173,37 @@ public class MainMenu extends AppCompatActivity {
         public void onStart() {
             super.onStart();
             ((TextView)getDialog().findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
+        }
+    }
+
+    static public class AboutDialog extends DialogFragment {
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            LayoutInflater factory = LayoutInflater.from(getActivity());
+            View layout = factory.inflate(R.layout.dlg_about, null);
+
+            try {
+                ((TextView) layout.findViewById(R.id.lbl_header)).setText(
+                        getActivity().getResources().getString(R.string.app_name) + " " +
+                                getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionName);
+            }
+            catch(Exception e) {
+            }
+
+            ((TextView)layout.findViewById(R.id.lbl_thanks)).setText("Anton Chivchalov, Ruben Reis, Luis Montes de Oca, Paul Kruk, Marcin Budziński, Rados Devadesátdva, Stanislav Kolesnik, Matthias Wirobski, Tiago Aguiar, Abel Puertas, Pablo Carrasco Jr, Andrea Isaza, Vytautas Virganavičius, Mrydka Śmigacz, Eugen Betke, Pedro Paulo");
+
+            return new AlertDialog.Builder(getActivity())
+                    .setView(layout)
+                    //.setMessage(R.string.msg_revision_notes_1_4_1)
+                    .setPositiveButton(R.string.btn_ok, null).create();
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            ((TextView)getDialog().findViewById(R.id.lbl_github_url)).setMovementMethod(LinkMovementMethod.getInstance());
         }
     }
 
